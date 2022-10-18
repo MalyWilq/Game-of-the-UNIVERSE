30e80a81c80545f30251595c764ed6f3e10ad154
MalyWilq <wilczan.moscicki@gmail.com>
2022-10-18 19:26:52 +0200

    Duże zmiany. Dodaje statystkyki

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import time
 import math
+import random
 
 
 HP = 100
 DEF = 100
+SPEED = 100
+STRG = 100
+#STRG - siła
+STRGZ = 100
+#STRGZ - siła zbrojna
+INTL = 100
+#INTL - inteligencja
 END = 0
 character = []
 
 
 
 def start():
-    print('Game of UNIVERSE v1.0 alpha\n')
+    print('Game of UNIVERSE v1.2\n')
     print('czy chcesz zacząć grę?')
     odp = input()
     if odp == 'tak':
@@ -18,6 +26,12 @@ def start():
 
 def gcreator():
     global character
+    global HP
+    global DEF
+    global SPEED
+    global STRG
+    global STRGZ
+    global INTL
     gatunek = ['Człowiek', 'Kree', 'Elf', 'Bio-cyborg']
     ccztery = ['']
     print('wybież gatunek')
@@ -30,6 +44,12 @@ def gcreator():
         if odp2 != 'tak':
             gcreator()
         else:
+            HP = 100
+            DEF = 0
+            SPEED = 50
+            STRG = 50
+            STRGZ = 50
+            INTL = 50
             character.append(gatunek[0])
             kcreator(character)
     elif odp == '2':
@@ -38,6 +58,12 @@ def gcreator():
         if odp2 != 'tak':
             gcreator()
         else:
+            HP = 100
+            DEF = 50
+            SPEED = 40
+            STRG = 40
+            STRGZ = 100
+            INTL = 100
             character.append(gatunek[1])
             kcreator(character)
     elif odp == '3':
@@ -46,6 +72,12 @@ def gcreator():
         if odp2 != 'tak':
             gcreator()
         else:
+            HP = 80
+            DEF = 10
+            SPEED = 70
+            STRG = 30
+            STRGZ = 30
+            INTL = 40
             character.append(gatunek[3])
             kcreator(character)
     elif odp == '4':
@@ -54,6 +86,12 @@ def gcreator():
         if odp2 != 'tak':
             gcreator()
         else:
+            HP = 200
+            DEF = 200
+            SPEED = 20
+            STRG = 100
+            STRGZ = 110
+            INTL = 50
             character.append(gatunek[3])
             kcreator(character)
     elif odp == '5':
@@ -72,10 +110,16 @@ def gcreator():
 
 def kcreator(postać):
     global character
+    global HP
+    global DEF
+    global SPEED
+    global STRG
+    global STRGZ
+    global INTL
     klasyC = ['Assasin', 'Mag', 'Ninja', 'Bogacz', 'Żołnierz']
     klasyK = ['Inżynier', 'Strzelec', 'Pilot', ]
     klasyE = ['Czarownik', 'Wojownik', 'Łucznik']
-    klasyBC = ['Czołg', 'Laserowy strzelec']
+    klasyBC = ['Czołg', 'Biegacz']
     if postać[0] == 'Człowiek':
         print('Wybierz klasę')
         for i in range(len(klasyC)):
@@ -86,6 +130,12 @@ def kcreator(postać):
             if odp2 != 'tak':
                 kcreator(character)
             else:
+                HP = 110
+                DEF = 90
+                SPEED = 50
+                STRG = 70
+                STRGZ = 50
+                INTL = 50
                 character.append(klasyC[0])
                 odp3 = input(f'twoja postać to {character}. tak? ')
                 if odp3 != 'tak':
@@ -99,6 +149,12 @@ def kcreator(postać):
             if odp2 != 'tak':
                 kcreator(character)
             else:
+                HP = 80
+                DEF = 10
+                SPEED = 10
+                STRG = 10
+                STRGZ = 67
+                INTL = 80
                 character.append(klasyC[1])
                 odp3 = input(f'twoja postać to {character}. tak? ')
                 if odp3 != 'tak':
@@ -112,6 +168,12 @@ def kcreator(postać):
             if odp2 != 'tak':
                 kcreator(character)
             else:
+                HP = 60
+                DEF = 10
+                SPEED = 70
+                STRG = 70
+                STRGZ = 70
+                INTL = 50
                 character.append(klasyC[2])
                 odp3 = input(f'twoja postać to {character}. tak? ')
                 if odp3 != 'tak':
@@ -125,6 +187,12 @@ def kcreator(postać):
             if odp2 != 'tak':
                 kcreator(character)
             else:
+                HP = 200
+                DEF = 150
+                SPEED = 10
+                STRG = 10
+                STRGZ = 200
+                INTL = 60
                 character.append(klasyC[3])
                 odp3 = input(f'twoja postać to {character}. tak? ')
                 if odp3 != 'tak':
@@ -138,6 +206,12 @@ def kcreator(postać):
             if odp2 != 'tak':
                 kcreator(character)
             else:
+                HP = 100
+                DEF = 200
+                SPEED = 60
+                STRG = 70
+                STRGZ = 80
+                INTL = 50
                 character.append(klasyC[4])
                 odp3 = input(f'twoja postać to {character}. tak? ')
                 if odp3 != 'tak':
@@ -156,6 +230,12 @@ def kcreator(postać):
             if odp2 != 'tak':
                 kcreator(character)
             else:
+                HP = 100
+                DEF = 120
+                STRG = 60
+                STRGZ = 80
+                SPEED = 40
+                INTL = 100
                 character.append(klasyK[0])
                 odp3 = input(f'twoja postać to {character}. tak? ')
                 if odp3 != 'tak':
@@ -164,11 +244,17 @@ def kcreator(postać):
                 else:
                     historia(character)
         if odp == '2':
-            odp2 = input(f'Wybrano klasę {klasyC[1]}. Czy napewno? ')
+            odp2 = input(f'Wybrano klasę {klasyK[1]}. Czy napewno? ')
             if odp2 != 'tak':
                 kcreator(character)
             else:
-                character.append(klasyC[1])
+                HP = 100
+                DEF = 70
+                STRG = 60
+                STRGZ = 90
+                SPEED = 40
+                INTL = 60
+                character.append(klasyK[1])
                 odp3 = input(f'twoja postać to {character}. tak? ')
                 if odp3 != 'tak':
                     character.remove(0, 1)
@@ -176,11 +262,17 @@ def kcreator(postać):
                 else:
                     historia(character)
         if odp == '3':
-            odp2 = input(f'Wybrano klasę {klasyC[2]}. Czy napewno? ')
+            odp2 = input(f'Wybrano klasę {klasyK[2]}. Czy napewno? ')
             if odp2 != 'tak':
                 kcreator(character)
             else:
-                character.append(klasyC[2])
+                HP = 100
+                DEF = 50
+                STRG = 50
+                STRGZ = 70
+                SPEED = 50
+                INTL = 60
+                character.append(klasyK[2])
                 odp3 = input(f'twoja postać to {character}. tak? ')
                 if odp3 != 'tak':
                     character.remove(0, 1)
@@ -197,6 +289,12 @@ def kcreator(postać):
             if odp2 != 'tak':
                 kcreator(character)
             else:
+                HP = 100
+                DEF = 80
+                STRG = 40
+                STRGZ = 100
+                SPEED = 60
+                INTL = 200
                 character.append(klasyE[0])
                 odp3 = input(f'twoja postać to {character}. tak? ')
                 if odp3 != 'tak':
@@ -209,6 +307,12 @@ def kcreator(postać):
             if odp2 != 'tak':
                 kcreator(character)
             else:
+                HP = 100
+                DEF = 20
+                STRG = 70
+                STRGZ = 50
+                SPEED = 60
+                INTL = 40
                 character.append(klasyE[1])
                 odp3 = input(f'twoja postać to {character}. tak? ')
                 if odp3 != 'tak':
@@ -221,6 +325,12 @@ def kcreator(postać):
             if odp2 != 'tak':
                 kcreator(character)
             else:
+                HP = 100
+                DEF = 10
+                STRG = 50
+                STRGZ = 80
+                SPEED = 80
+                INTL = 40
                 character.append(klasyE[2])
                 odp3 = input(f'twoja postać to {character}. tak? ')
                 if odp3 != 'tak':
@@ -238,6 +348,12 @@ def kcreator(postać):
             if odp2 != 'tak':
                 kcreator(character)
             else:
+                HP = 200
+                DEF = 300
+                STRG = 200
+                STRGZ = 10
+                SPEED = 10
+                INTL = 30
                 character.append(klasyBC[0])
                 odp3 = input(f'twoja postać to {character}. tak? ')
                 if odp3 != 'tak':
@@ -250,6 +366,12 @@ def kcreator(postać):
             if odp2 != 'tak':
                 kcreator(character)
             else:
+                HP = 200
+                DEF = 50
+                STRG = 50
+                STRGZ = 10
+                SPEED = 200
+                INTL = 60
                 character.append(klasyBC[1])
                 odp3 = input(f'twoja postać to {character}. tak? ')
                 if odp3 != 'tak':
@@ -257,6 +379,18 @@ def kcreator(postać):
                     gcreator()
                 else:
                     historia(character)
+#        if odp == '3':
+#            odp2 = input(f'Wybrano klasę {klasyBC[2]}. Czy napewno? ')
+#            if odp2 != 'tak':
+#                kcreator(character)
+#            else:
+#                character.append(klasyBC[1])
+#                odp3 = input(f'twoja postać to {character}. tak? ')
+#                if odp3 != 'tak':
+#                    character.remove(0, 1)
+#                    gcreator()
+#                else:
+#                    historia(character)
 
 
 
@@ -294,9 +428,27 @@ def historia(ty):
     print('J.O.P.U.L 2(Jednostka ochrony placówek Universe Labs): Natychmiast!')
     input('<wciśnij enter>')
     print()
-    gra()
+    nameg()
 
-def gra():
+def nameg():
+    global character
+    name = input('Jesteś eksperymentem404. Wpisz własne imie:')
+    if name == '':
+        gra()
+    elif name == 'W123':
+        pass
+    elif name == 'MWDevTool':
+        pass
+    else:
+        odp = input(f'wybrano imię {name}. Czy potwierdzasz: ')
+        if odp != 'tak':
+            nameg()
+        else:
+            character.append(name)
+            print(f'Jesteś: Rasa {character[0]}, klasa {character[1]}, imię {character[2]}')
+
+
+def historia2(ty):
     pass
 
 # Press the green button in the gutter to run the script.
